720b22a9deb3d534477e22e65013cf160d0b2d34
rizwanamalak <rizuyasmin@gmail.com>
2020-01-21 10:59:52 +0530

    first commit

---
+++ src/test/java/firstTest/FirstTest.java
@@ -0,0 +1,28 @@
+package firstTest;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class FirstTest {
+@Test
+public void validateFirstTest() throws IOException {
+	WebDriver wd= new FirefoxDriver();
+	wd.manage().window().maximize();
+	wd.get("https://www.bing.com");
+	WebElement data=wd.findElement(By.name("q"));
+	data.sendKeys("Chennai");
+	data.sendKeys(Keys.ENTER);
+	File src=((TakesScreenshot)wd).getScreenshotAs(OutputType.FILE);
+	FileUtils.copyFile(src,new File("C:\\workspace\\GitHub\\Screenshot\\capture.jpg"));
+}
+}
